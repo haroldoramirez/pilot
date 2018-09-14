be2539ce0e5861c905ad9284d5eeb05637ecc659
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-09-14 14:04:52 -0300

    self-register: add preserve state at TypeCNPJ

--- packages/pilot/src/containers/SelfRegister/TypeCNPJ/index.js
+++ packages/pilot/src/containers/SelfRegister/TypeCNPJ/index.js
@@ -17,11 +17,13 @@ const step = 'type-cnpj'
 const isRequired = t => requiredValidation(t('pages.self_register.required_error'))
 
 class SelfRegisterTypeCNPJ extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
 
     this.state = {
-      cnpj: '',
+      formData: {
+        cnpj: props.registerData.cnpj,
+      },
     }
 
     this.handleMaskField = handleMaskField.bind(this)
@@ -50,7 +52,7 @@ class SelfRegisterTypeCNPJ extends Component {
         <Form
           className={style.fillWidth}
           data={{
-            cnpj: this.state.cnpj,
+            cnpj: this.state.formData.cnpj,
           }}
           onSubmit={onSubmit}
           validateOn="blur"
@@ -76,8 +78,14 @@ class SelfRegisterTypeCNPJ extends Component {
 }
 
 SelfRegisterTypeCNPJ.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  registerData: PropTypes.object,
   onSubmit: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
+}
+
+SelfRegisterTypeCNPJ.defaultProps = {
+  registerData: {},
 }
 
 export default SelfRegisterTypeCNPJ
